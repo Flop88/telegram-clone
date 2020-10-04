0b3cc0ccaa6eef1768d35842d4609ffe35aade8e
Flop88 <wwwqwwwqwwwqwwwq@gmail.com>
2020-10-04 11:41:46 +0300

    Refactoring

---
+++ app/src/main/java/ru/mvlikhachev/telegram/ui/fragments/ChatsFragment.kt
@@ -0,0 +1,10 @@
+package ru.mvlikhachev.telegram.ui.fragments
+
+import ru.mvlikhachev.telegram.R
+
+class ChatsFragment : BaseFragment(R.layout.fragment_chat) {
+
+    override fun onResume() {
+        super.onResume()
+    }
+}
---
+++ app/src/main/java/ru/mvlikhachev/telegram/ui/fragments/SettingsFragment.kt
@@ -0,0 +1,12 @@
+package ru.mvlikhachev.telegram.ui.fragments
+
+
+import ru.mvlikhachev.telegram.R
+
+class SettingsFragment : BaseFragment(R.layout.fragment_settings) {
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+}
